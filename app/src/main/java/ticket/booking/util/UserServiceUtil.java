fbf0de47fb2993b877ac0ae7f1c1e026c2b29543
md faiyaz ansari <154304294+ansfaiz@users.noreply.github.com>
2025-08-21 22:30:50 +0530

    Update UserServiceUtil.java

--- app/src/main/java/ticket/booking/util/UserServiceUtil.java
+++ app/src/main/java/ticket/booking/util/UserServiceUtil.java
@@ -1,5 +1,11 @@
 package ticket.booking.util;
-
+/*
+ * Copyright (c) 2025 Ansfaiz
+ *
+ * Licensed under the MIT License.
+ * You may obtain a copy of the License at
+ * https://opensource.org/licenses/MIT
+ */
 import org.mindrot.jbcrypt.BCrypt;
 
 public class UserServiceUtil
